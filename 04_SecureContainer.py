7090812b90a2035d5eabb977ae971c4fb8b4cc2f
magnus <namoruma@gmail.com>
2019-12-04 13:33:01 +0100

    Reformating code: Shorter and more efficient

--- 04_SecureContainer.py
+++ 04_SecureContainer.py
@@ -1,9 +1,5 @@
 from collections import Counter
 
-"""
-PART 1:
-"""
-
 part1 = 0
 part2 = 0
 minimum = 272091
@@ -11,32 +7,24 @@ maximum = 815432 + 1
 
 
 def check_pin(pin):
-    retval = False
+    firstpart = secondpart = False
     for i in range(1, len(pin)):
-        if pin[i] < pin[i - 1]: return False
-        if pin[i] == pin[i - 1]: retval = True
-    return retval
+        if pin[i] < pin[i - 1]:
+            return False, False
+        if pin[i] == pin[i - 1]:
+            firstpart = True
 
+    if any([3 > v > 1 for k, v in Counter(pin).items()]):  # group and count the items
+        secondpart = True
 
-"""
-PART 2: 
-"""
-
-
-def check_pin_2(pin):
-    retval = False
-    for i in range(1, len(pin)):
-        if pin[i] < pin[i - 1]: return False
-        if any([3 > v > 1 for k, v in Counter(pin).items()]): # group and count the items
-            retval = True
-
-    return retval
+    return firstpart, secondpart
 
 
 for number in range(minimum, maximum):
-    if check_pin(str(number)):
+    firstpart, secondpart = check_pin(str(number))
+    if firstpart:
         part1 += 1
-    if check_pin_2(str(number)):
+    if secondpart:
         part2 += 1
 
 print(part1, part2)
