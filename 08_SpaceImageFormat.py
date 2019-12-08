951e1c6e887aa63720f856d76fa09ec000c0c395
magnus <namoruma@gmail.com>
2019-12-08 12:11:00 +0100

    Day 08

---
+++ 08_SpaceImageFormat.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+PART 1:
+"""
+
+S_W = 25
+S_T = 6
+lsiz = S_W*S_T
+
+rwdat = open("data/08_input").readline()
+pdig = [int(x) for x in rwdat]
+
+layers = [ pdig[i:i+lsiz] for i in range(0, len(pdig), lsiz) ]
+
+checkvals = [(x.count(0),(x.count(2)*x.count(1))) for x in layers if x.count(0) != 0]
+print("Checksum is:", min(checkvals, key=lambda x: x[0])[1])
+
+"""
+PART 2:
+"""
+# Render the layers from back to front and overwrite the pixels?
+
+TRANSPARENT = 2
+BLACK = 0
+WHITE = 1
+finimg = [0]*lsiz
+for l in reversed(layers):
+    for p in range(len(l)):
+        if l[p] == TRANSPARENT:
+            continue
+        elif l[p] == WHITE or l[p] == BLACK:
+            finimg[p] = l[p]
+
+for s in range(lsiz):
+    letter = '\33[100m' + " " + '\033[0m' if finimg[s] == 1 else '\33[107m' + " " + '\033[0m'
+    print(letter, end="")
+    if (s+1)%S_W == 0:
+        print()
+
+
