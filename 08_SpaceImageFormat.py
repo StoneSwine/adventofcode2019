4af8136b48039725c8470b0e7c6f737173f811fc
magnus <namoruma@gmail.com>
2019-12-08 12:18:48 +0100

    Adding comments

--- 08_SpaceImageFormat.py
+++ 08_SpaceImageFormat.py
@@ -11,15 +11,19 @@ lsiz = S_W*S_T
 rwdat = open("data/08_input").readline()
 pdig = [int(x) for x in rwdat]
 
+# split the array in lsiz chunks (layers)
 layers = [ pdig[i:i+lsiz] for i in range(0, len(pdig), lsiz) ]
 
+# get all the nubmer of zeros and no.1*no.2 for all the layers
 checkvals = [(x.count(0),(x.count(2)*x.count(1))) for x in layers if x.count(0) != 0]
+
+# the layer with min. zeroes = checksum value
 print("Checksum is:", min(checkvals, key=lambda x: x[0])[1])
 
 """
 PART 2:
 """
-# Render the layers from back to front and overwrite the pixels?
+# Render the layers from back to front and overwrite the pixels
 
 TRANSPARENT = 2
 BLACK = 0
@@ -32,6 +36,7 @@ for l in reversed(layers):
         elif l[p] == WHITE or l[p] == BLACK:
             finimg[p] = l[p]
 
+# print the final image with colors
 for s in range(lsiz):
     letter = '\33[100m' + " " + '\033[0m' if finimg[s] == 1 else '\33[107m' + " " + '\033[0m'
     print(letter, end="")
